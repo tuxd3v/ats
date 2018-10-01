10a6c18498a7e3b89fd7d3bec76d19f4708fe88b
tuxd3v <tuxd3v@sapo.pt>
2018-10-01 17:15:04 +0100

    cosmetic changes

--- src/sleep.c
+++ src/sleep.c
@@ -11,7 +11,8 @@
 * unistd.h don't provide a msleep function( provide usleep ), but still,
 * Avoid to name convenctions from unistd.h, providing a termination "_c" on function name
 */
-static int msleep_c( lua_State *L ){
+static int msleep_c( lua_State *L )
+{
 	long msecs = lua_tointeger( L, -1 );
 	usleep( 1000 * msecs );
 	return 0;   /* No items returned */
@@ -21,14 +22,16 @@ static int msleep_c( lua_State *L ){
 * unistd.h provide a sleep function,
 * Avoid to name  convenctions from unistd.h, providing a termination "_c" on function name
 */
-static int sleep_c( lua_State *L ){
+static int sleep_c( lua_State *L )
+{
 	long secs = lua_tointeger( L, -1 );
 	sleep( secs );
 	return 0;   /* No items returned */
 }
 
 /* Register both functions */
-int luaopen_sleep( lua_State *L ){
+int luaopen_sleep( lua_State *L )
+{
 	lua_register( L, "msleep", msleep_c );  
 	lua_register( L, "sleep", sleep_c );
 	return 0;
