3e5d4cd878e6ba17abfd4fa3aeb34ec507c2c402
JackGlobetrotter <jakob.dickert@outlook.com>
2019-12-03 01:13:25 +0100

    Update pwm1 file every second while fan is running

--- src/ats.c
+++ src/ats.c
@@ -466,7 +466,12 @@ static int loop_c( lua_State *L ){
 																												thermal_[ 1 ],
 																												pwm );
 			/* Sleeping with Fan ON until next cycle */
-			sleep( Rtimer[ temp ] );
+			// sleep( Rtimer[ temp ] );
+            for (int i = 0; i< Rtimer[ temp ]; i++)
+            {
+                setPwm(instant_ratio);
+                sleep(1);
+            }
 
 			/* Stop Fan */
 			setPwm( 0 );
